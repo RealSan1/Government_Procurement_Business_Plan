8b7a79be11f5c113efab29f8af92f67c14e737e8
San <soltks123@gmail.com>
2025-10-03 04:34:36 +0900

    env 수정

--- db.py
+++ db.py
@@ -1,13 +1,9 @@
 import os
-from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from urllib.parse import urlparse
-
-load_dotenv(dotenv_path="apikey.env")
 
 def get_conn():
     return psycopg2.connect(
-        os.environ["DATABASE_URL"],
+        os.environ["DATABASE_URL"],  # Render 환경 변수에서 가져옴
         cursor_factory=RealDictCursor
     )
