f272e4167bcbbba576f293141e67827057e8f7bf
San <soltks123@gmail.com>
2025-10-03 03:49:30 +0900

    시작 커맨드 수정

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uvicorn
-
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -90,5 +90,5 @@ def resources_page(request: Request):
     return templates.TemplateResponse("resources.html", {"request": request})
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=10000, reload=True)
-
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
