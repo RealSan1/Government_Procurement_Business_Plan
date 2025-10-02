b531d038c2cc2164ddd104f85fad531895a254f5
San <soltks123@gmail.com>
2025-10-03 03:37:17 +0900

    port 변경

--- main.py
+++ main.py
@@ -90,5 +90,5 @@ def resources_page(request: Request):
     return templates.TemplateResponse("resources.html", {"request": request})
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=10000, reload=True)
 
